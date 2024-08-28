610231fa38342303cc79715c9c2ee95fbf4ab1df
tom <tommckeown909@gmail.com>
2024-08-28 20:04:43 +0100

    1 - Two Sum and 2485 - Pivot Integer

---
+++ 1_two_sum/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        int firstNum = 0;
+        int secondNum = 0;
+        int[] sumValues = new int[2];
+        for (int i = 0;i < nums.Length; i++){
+            firstNum = nums[i];
+            Console.WriteLine("nums[i] = " + nums[i]);
+            for (int j = 0; j < nums.Length; j++){
+                if (j == i){
+                    continue;
+                }
+                secondNum = nums[j];
+                Console.WriteLine("nums[j] = " + nums[j]);
+                if (firstNum + secondNum == target){
+                    Console.WriteLine("index of sum values = " + i + j);
+                    sumValues[0] = i;
+                    sumValues[1] = j;
+                }
+            }
+        }
+        return sumValues;
+    }
+
+    public static void Main(string[] args){
+        //Test cases
+        Solution sol = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = sol.TwoSum(nums, target);
+        Console.WriteLine("[" + result[0] + ", " + result[1] + "]");
+    }
+}
---
+++ 2485_pivot_integer/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution {
+    public int PivotInteger(int n) {
+        int runningTotal = 0;
+        int sum = n * (n + 1) / 2;
+        for (int i = n; i > 0; i--){            
+            runningTotal += i;
+            Console.WriteLine($"runningTotal = {runningTotal}, sum = {sum}, i = {i}");
+            if (runningTotal == sum){
+                return i;
+            }
+            sum -= i;
+        }
+        return -1;
+    }
+
+    public static void Main(string[] args){
+        //Test cases
+        Solution sol = new Solution();
+        Console.WriteLine(sol.PivotInteger(8)); //6
+        Console.WriteLine(sol.PivotInteger(1)); //1
+        Console.WriteLine(sol.PivotInteger(4)); //-1
+    }
+}
